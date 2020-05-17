0505aaba90c42ba1a97802b3e19a8bb1937dfdc8
Thomas Gohard <thomasgohard@users.noreply.github.com>
2020-05-17 12:57:36 -0400

    Initial code

---
+++ FilesAnalyser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace thomasgohard.FilesAnalyser {
+	class FilesAnalyser {
+		static int Main(string[] args) {
+			string rootPathToAnalyse;
+			
+			if (args.Length != 1) {
+				Console.WriteLine("Invalid number of arguments: Please provide a path to a directory to analyse.");
+				return 1;
+			}
+
+			rootPathToAnalyse = args[0];
+			try {
+				FileAttributes pathAttributes = File.GetAttributes(rootPathToAnalyse);
+
+				if ((pathAttributes & FileAttributes.Directory) != FileAttributes.Directory) {
+					Console.WriteLine("Path doesn't point to a directory: Please provide a path to a directory to analyse.");
+					return 1;
+				}
+			} catch(Exception e) {
+				Console.WriteLine(e.GetType().Name + ": " + e.Message);
+				return 1;
+			}
+
+			Console.Write("Analysing " + Path.GetFullPath(rootPathToAnalyse) + ": ");
+			
+			try {
+				string[] filePathsToAnalyse = Directory.GetFiles(rootPathToAnalyse);
+				Console.WriteLine(filePathsToAnalyse.Length + " files found.");
+
+				foreach(string filePathToAnalyse in filePathsToAnalyse) {
+					Console.WriteLine(Path.GetFullPath(filePathToAnalyse));
+				}
+			} catch(Exception e) {
+				Console.WriteLine(e.GetType().Name + ": " + e.Message);
+				return 1;
+			}
+
+			return 0;
+		}
+	}
+}
