3aefd06ba04cb31e30c50dea37ce3ae1dc1f7081
Thomas Gohard <thomasgohard@users.noreply.github.com>
2020-05-24 09:49:26 -0400

    Add recursive handling

--- FilesAnalyser.cs
+++ FilesAnalyser.cs
@@ -8,13 +8,28 @@ namespace thomasgohard.FilesAnalyser {
 		static private StreamWriter outputStream;
 		static int Main(string[] args) {
 			string rootPathToAnalyse;
+			bool recursive = false;
 			
-			if (args.Length != 1) {
-				Console.WriteLine("Invalid number of arguments: Please provide a path to a directory to analyse.");
+			if (args.Length < 1) {
+				Console.WriteLine("Invalid number of arguments: Please provide a path to a directory to analyse at a minimum.");
 				return 1;
 			}
 
-			rootPathToAnalyse = args[0];
+			for (int i = 0; i < (args.Length - 1); i++) {
+				if (args[i].Substring(0, 1) == "-") {	// option flag
+					if (args[i].Substring(1) == "r") {
+						recursive = true;
+					}
+				}
+			}
+			rootPathToAnalyse = args[args.Length - 1];
+
+			Console.Write("Analysing path " + rootPathToAnalyse + " in ");
+			if (!recursive) {
+				Console.Write("non-");
+			}
+			Console.WriteLine("recursive mode.");
+
 			try {
 				FileAttributes pathAttributes = File.GetAttributes(rootPathToAnalyse);
 
@@ -29,10 +44,9 @@ namespace thomasgohard.FilesAnalyser {
 
 			Console.Write("Analysing " + Path.GetFullPath(rootPathToAnalyse) + ": ");
 			try {
-				//outputStream = new StreamWriter("file-inventory.csv", false, Encoding.UTF8);
 				using(outputStream = new StreamWriter("file-inventory.csv", false, Encoding.UTF8)) {
 					outputStream.WriteLine("Name,Path,Type,Size,Hash");
-					AnalyseDirectory(rootPathToAnalyse);
+					AnalyseDirectory(rootPathToAnalyse, recursive);
 				}
 			} catch(Exception e) {
 				Console.WriteLine(e.GetType().Name + ": " + e.Message);
@@ -42,7 +56,7 @@ namespace thomasgohard.FilesAnalyser {
 			return 0;
 		}
 
-		static void AnalyseDirectory(string rootPathToAnalyse) {
+		static void AnalyseDirectory(string rootPathToAnalyse, bool recursive) {
 			try {
 				DirectoryInfo pathToAnalyseInfo = new DirectoryInfo(rootPathToAnalyse);
 				FileInfo[] filesToAnalyseInfo = pathToAnalyseInfo.GetFiles();
@@ -51,13 +65,14 @@ namespace thomasgohard.FilesAnalyser {
 				Console.WriteLine(filesToAnalyseInfo.Length + " files and " + directoriesToAnalyseInfo.Length + " directories found.");
 
 				foreach(FileInfo fileToAnalyseInfo in filesToAnalyseInfo) {
-					//Console.WriteLine(fileToAnalyseInfo.Name + "," + fileToAnalyseInfo.DirectoryName + "," + fileToAnalyseInfo.Extension + "," + fileToAnalyseInfo.Length + "," + getFileHash(fileToAnalyseInfo.FullName));
 					outputStream.WriteLine(fileToAnalyseInfo.Name + "," + fileToAnalyseInfo.DirectoryName + "," + fileToAnalyseInfo.Extension + "," + fileToAnalyseInfo.Length + "," + getFileHash(fileToAnalyseInfo.FullName));
 				}
 
-				foreach(DirectoryInfo directoryToAnalyseInfo in directoriesToAnalyseInfo) {
-					Console.Write("Analysing " + Path.GetFullPath(rootPathToAnalyse) + ": ");
-					AnalyseDirectory(directoryToAnalyseInfo.FullName);
+				if (recursive) {
+					foreach(DirectoryInfo directoryToAnalyseInfo in directoriesToAnalyseInfo) {
+						Console.Write("Analysing " + Path.GetFullPath(rootPathToAnalyse) + ": ");
+						AnalyseDirectory(directoryToAnalyseInfo.FullName, recursive);
+					}
 				}
 			} catch(Exception e) {
 				throw;
